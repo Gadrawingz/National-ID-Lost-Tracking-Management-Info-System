285db26062a3136bab2d5f7bc0d16e82571c25b5
Gad Iradufasha <50759844+Gadrawingz@users.noreply.github.com>
2022-04-21 23:27:28 +0200

    Updating  file dealing with database
    Updating file handling database - Semi-final

--- application/data/IDAllData.php
+++ application/data/IDAllData.php
@@ -15,7 +15,7 @@ class IDAllData extends DBConfig {
 		$this->con= $obj->connect();
 	}
 
-	public function postFoundID($id_names, $id_num, $id_dob, $id_sex, $is_dist, $is_sect, $image_file, $u_location, $u_fn, $u_ln, $contact) {
+	public function postFoundID($id_names, $id_num, $id_dob, $id_sex, $is_dist, $is_sect, $image_file, $u_location, $v_id) {
 
 		// Taking care of file name
         $format = new AppConfig();
@@ -39,14 +39,14 @@ class IDAllData extends DBConfig {
         if(!in_array($file_type, $mimes)){
         	return " Uploaded file is not image supported!";
 
-        } else if($id_names=='' || $id_num=='' || $id_dob=='' || $id_sex=='' || $is_dist=='' || $is_sect=='' || $image_file=='' || $u_location=='' || $u_fn=='' || $u_ln=='' || $contact==''){
+        } else if($id_names=='' || $id_num=='' || $id_dob=='' || $id_sex=='' || $is_dist=='' || $is_sect=='' || $image_file=='' || $u_location=='' || $v_id=='' ){
             return '<span class="c-white-red"> Some fields are empty!</span>';
         	
         } else if(strlen($id_num)!=16){
             return '<span class="c-white-red">ID should have 16 numbers!</span>';
 
-        } else if(!preg_match("/^07[2,3,8,9]{1}\d{7}$/", $contact)) {
-            return '<span class="c-white-red"> Invalid Phone number</span>';
+        } else if($v_id=='0'){
+            return '<span class="c-white-red">Account is not setup!</span>';
 
         } else if($idst->fetchColumn()!=0) {
             return '<span class="c-white-red">This National ID already posted!</span>';
@@ -55,7 +55,7 @@ class IDAllData extends DBConfig {
         	$uploadFilePath = '../../uploads/'.basename($file_name);
         	move_uploaded_file($file_temp, $uploadFilePath);
         	
-        	$sql= "INSERT INTO `found`(`nid_names`, `nid_number`, `nid_dob`, `nid_sex`, `issued_dist`, `issued_sect`, `nid_image`, `found_location`, `finder_fn`, `finder_ln`, `finder_contact`) VALUES ('".addslashes($id_names)."', '$id_num', '$id_dob', '$id_sex', '$is_dist', '$is_sect', '".$file_name."', '".addslashes($u_location)."', '".addslashes($u_fn)."', '".addslashes($u_ln)."', '$contact')";
+        	$sql= "INSERT INTO `found`(`nid_names`, `nid_number`, `nid_dob`, `nid_sex`, `issued_dist`, `issued_sect`, `nid_image`, `found_location`, `user_id`) VALUES ('".addslashes($id_names)."', '$id_num', '$id_dob', '$id_sex', '$is_dist', '$is_sect', '".$file_name."', '".addslashes($u_location)."', '$v_id')";
         	$query= $this->con->prepare($sql);
         	$query->execute();
         	$count= $query->rowCount();
@@ -75,9 +75,8 @@ class IDAllData extends DBConfig {
         }
 	}
 
-
-
-	public function postLostID($id_names, $id_num, $id_dob, $id_sex, $is_dist, $is_sect, $image_file, $from, $to, $date, $reward, $contact) {
+    // @donnekt @gadrawingz
+	public function postLostID($id_names, $id_num, $id_dob, $id_sex, $is_dist, $is_sect, $image_file, $from, $to, $date, $reward, $v_id) {
 
 		// Taking care of file name
         $format = new AppConfig();
@@ -100,23 +99,23 @@ class IDAllData extends DBConfig {
         if(!in_array($file_type, $mimes)){
         	return " Uploaded file is not image supported!";
 
-        } else if($id_names=='' || $id_num=='' || $id_dob=='' || $id_sex=='' || $is_dist=='' || $is_sect=='' || $image_file=='' || $from=='' || $to=='' || $reward=='' || $contact==''){
+        } else if($id_names=='' || $id_num=='' || $id_dob=='' || $id_sex=='' || $is_dist=='' || $is_sect=='' || $image_file=='' || $from=='' || $to=='' || $reward=='' || $v_id==''){
         	return '<span class="c-white-red"> Some fields are empty!</span>';
             
         } else if(strlen($id_num)!=16){
             return '<span class="c-white-red">ID should have 16 numbers!</span>';
 
+        } else if($v_id=='0'){
+            return '<span class="c-white-red">Account is not setup!</span>';
+
         } else if($idst->fetchColumn()!=0) {
             return '<span class="c-white-red">This National ID already posted!</span>';
 
-        } else if(!preg_match("/^07[2,3,8,9]{1}\d{7}$/", $contact)) {
-            return '<span class="c-white-red"> Invalid Phone number</span>';
-            
         } else {
         	$uploadFilePath = '../../uploads/'.basename($file_name);
         	move_uploaded_file($file_temp, $uploadFilePath);
         	
-        	$sql= "INSERT INTO `lost`(`nid_names`, `nid_number`, `nid_dob`, `nid_sex`, `issued_dist`, `issued_sect`, `nid_image`, `lost_from`, `lost_to`, `lost_date`, `reward`, `loser_contact`) VALUES ('".addslashes($id_names)."', '$id_num', '$id_dob', '$id_sex', '$is_dist', '$is_sect', '".$file_name."', '".addslashes($from)."', '".addslashes($to)."', '$date', '$reward', '$contact')";
+        	$sql= "INSERT INTO `lost`(`nid_names`, `nid_number`, `nid_dob`, `nid_sex`, `issued_dist`, `issued_sect`, `nid_image`, `lost_from`, `lost_to`, `lost_date`, `reward`, `user_id`) VALUES ('".addslashes($id_names)."', '$id_num', '$id_dob', '$id_sex', '$is_dist', '$is_sect', '".$file_name."', '".addslashes($from)."', '".addslashes($to)."', '$date', '$reward', '$v_id')";
         	$query= $this->con->prepare($sql);
         	$query->execute();
         	$count= $query->rowCount();
@@ -127,16 +126,17 @@ class IDAllData extends DBConfig {
 
 
 
-	// ALL ABOUT LOST
+	// ALL ABOUT LOST @donnekt @gadrawingz
 	public function viewAllLostIDs() {
-        $sql = "SELECT lo.nid_names, lo.nid_number AS national_id, lo.nid_dob, lo.nid_sex, lo.issued_dist, lo.issued_sect, lo.nid_image, lo.lost_from, lo.lost_to, lo.lost_date, lo.reward, lo.loser_contact, lo.action_date, lc.is_found, lc.is_received FROM lost lo LEFT JOIN lost_check lc ON lc.nid_number=lo.nid_number WHERE lc.nid_number IS NULL ORDER BY nid_names ASC LIMIT 20";
+        $sql = "SELECT lo.id_pk, lo.nid_names, lo.nid_number AS national_id, lo.nid_dob, lo.nid_sex, lo.issued_dist, lo.issued_sect, lo.nid_image, lo.lost_from, lo.lost_to, lo.lost_date, lo.reward, lo.action_date, us.user_id AS account_id, us.full_name AS account_names,  us.telephone AS account_tel, lc.is_found, lc.is_received FROM lost lo LEFT JOIN lost_check lc ON lc.nid_number=lo.nid_number LEFT JOIN user us ON us.user_id=lo.user_id WHERE lc.nid_number IS NULL ORDER BY nid_names ASC LIMIT 25";
         $stmt= $this->con->prepare($sql);
         $stmt->execute();
         return $stmt;
     }
 
-    public function viewAllJoinedLostIDs() {
-        $sql = "SELECT lo.nid_names, lo.nid_number AS national_id, lo.nid_dob, lo.nid_sex, lo.issued_dist, lo.issued_sect, lo.nid_image, lo.lost_from, lo.lost_to, lo.lost_date, lo.reward, lo.loser_contact, lo.action_date, lc.is_found, lc.is_received FROM lost lo LEFT JOIN lost_check lc ON lc.nid_number=lo.nid_number ORDER BY nid_names ASC LIMIT 50";
+    // @donnekt @gadrawingz
+    public function viewAllMergedLostIDs() {
+        $sql = "SELECT lo.id_pk, lo.nid_names, lo.nid_number AS national_id, lo.nid_dob, lo.nid_sex, lo.issued_dist, lo.issued_sect, lo.nid_image, lo.lost_from, lo.lost_to, lo.lost_date, lo.reward, lo.action_date, us.user_id AS account_id, us.full_name AS account_names,  us.telephone AS account_tel, lc.is_found, lc.is_received FROM lost lo LEFT JOIN lost_check lc ON lc.nid_number=lo.nid_number LEFT JOIN user us ON us.user_id=lo.user_id ORDER BY lo.nid_names ASC LIMIT 50";
         $stmt= $this->con->prepare($sql);
         $stmt->execute();
         return $stmt;
@@ -177,7 +177,7 @@ class IDAllData extends DBConfig {
      * ***********************************/
 
 	public function viewAllFoundIDs() {
-        $sql = "SELECT fo.nid_names, fo.nid_number AS national_id, fo.nid_dob, fo.nid_sex, fo.issued_dist, fo.issued_sect, fo.nid_image, fo.finder_fn, fo.found_location, fo.finder_ln, fo.finder_contact, fo.action_date, lc.is_found, lc.has_reward, lc.is_received FROM found fo LEFT JOIN lost_check lc ON lc.nid_number=fo.nid_number WHERE lc.nid_number IS NOT NULL ORDER BY fo.nid_names ASC LIMIT 20;";
+        $sql = "SELECT fo.id_pk, fo.nid_names, fo.nid_number AS national_id, fo.nid_dob, fo.nid_sex, fo.issued_dist, fo.issued_sect, fo.nid_image, fo.found_location, fo.action_date, fo.user_id AS account_id, us.full_name AS account_names, us.telephone AS account_tel, lc.is_found, lc.has_reward, lc.is_received FROM found fo LEFT JOIN lost_check lc ON lc.nid_number=fo.nid_number LEFT JOIN user us ON us.user_id=fo.user_id WHERE lc.nid_number IS NOT NULL ORDER BY fo.nid_names ASC LIMIT 25";
         $stmt= $this->con->prepare($sql);
         $stmt->execute();
         return $stmt;
@@ -212,6 +212,7 @@ class IDAllData extends DBConfig {
         return $stmt;
     }
 
+    // @donnekt @gadrawingz
     public function countResultInFoundIDs($n) {
         $sql = "SELECT COUNT(*) FROM found WHERE nid_names LIKE '%$n%' ";
         $stmt= $this->con->query($sql)->fetchColumn();
@@ -219,4 +220,80 @@ class IDAllData extends DBConfig {
     }
 
 
+
+
+    // Create visitor account:
+    public function registerVisitor($names, $tel, $pass) {
+
+        // Check for existing user
+        $q = "SELECT COUNT(*) FROM `user` WHERE telephone='".$tel."' ";
+        $idsq = $this->con->prepare($q);
+        $idsq->execute();
+
+        if($idsq->fetchColumn()!=0) {
+            return '<span class="c-white-red">User with these credetintials exists!</span>';
+
+        } else if(!preg_match("/^07[2,3,8,9]{1}\d{7}$/", $tel)) {
+            return '<span class="c-white-red"> Invalid Phone number</span>';
+            
+        } else {
+            $sql= "INSERT INTO `user`(`full_name`, `telephone`, `password`) VALUES ('".addslashes($names)."', '$tel', '$pass')";
+            $query= $this->con->prepare($sql);
+            $query->execute();
+            $count= $query->rowCount();
+
+            return "User account is created!";
+        }
+    }
+
+    public function registeredVisitors() {
+        $sql = "SELECT * FROM user ORDER BY full_name ASC LIMIT 25";
+        $stmt= $this->con->prepare($sql);
+        $stmt->execute();
+        return $stmt; 
+    }
+
+    // @donnekt @gadrawingz
+    public function countAllUsers() {
+        $sql = "SELECT COUNT(*) FROM user ";
+        $stmt= $this->con->query($sql)->fetchColumn();
+        return $stmt;
+    }
+
+    public function disableUser($id) {
+        $sql="UPDATE user SET status='0' WHERE user_id='$id' ";
+        $query= $this->con->prepare($sql);
+        $query->execute();
+        $count= $query->rowCount();
+        return $count;
+    }
+
+    // @donnekt @gadrawingz
+    public function enableUser($id) {
+        $sql="UPDATE user SET status='1' WHERE user_id='$id' ";
+        $query= $this->con->prepare($sql);
+        $query->execute();
+        $count= $query->rowCount();
+        return $count;
+    }
+
+    // @donnekt @gadrawingz
+    public function verifyReceivedId($id) {
+        $sql="UPDATE lost_check SET has_reward='1' WHERE nid_number='$id' ";
+        $query= $this->con->prepare($sql);
+        $query->execute();
+        $count= $query->rowCount();
+        return $count;
+    }
+
+    public function cancelIDVerification($id) {
+        $sql="UPDATE lost_check SET has_reward='0' WHERE nid_number='$id' ";
+        $query= $this->con->prepare($sql);
+        $query->execute();
+        $count= $query->rowCount();
+        return $count;
+    }
+
+
 }
+?>
--- application/data/LoginData.php
+++ application/data/LoginData.php
@@ -29,6 +29,46 @@ class LoginData extends DBConfig {
         return $stmt;
     }
 
+    public function doVisitorLogin($telephone, $pass) {
+        $sql= "SELECT * FROM `user` WHERE telephone='$telephone' AND password='$pass' ";
+        $stmt= $this->con->prepare($sql);
+        $stmt->execute();
+        return $stmt;
+    }
+
+
+
+
+    // ################################
+    public function viewPSAccount($id) {
+        $sql = "SELECT * FROM police_station WHERE station_id='$id' ";
+        $stmt= $this->con->prepare($sql);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function updatePSAccount($id, $username, $password) {
+        $sql="UPDATE police_station SET username='$username', password='$password' WHERE station_id='$id' ";
+        $query= $this->con->prepare($sql);
+        $query->execute();
+        $count= $query->rowCount();
+        return $count;
+    }
+
+    public function viewUserAccount($id) {
+        $sql = "SELECT * FROM user WHERE user_id='$id' ";
+        $stmt= $this->con->prepare($sql);
+        $stmt->execute();
+        return $stmt;
+    }
+
+    public function updateUserAccount($id, $fn, $tel, $password) {
+        $sql="UPDATE user SET full_name='$fn', telephone='$tel', password='$password' WHERE user_id='$id' ";
+        $query= $this->con->prepare($sql);
+        $query->execute();
+        $count= $query->rowCount();
+        return $count;
+    }
 
    
 }
